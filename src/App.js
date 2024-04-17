99603ec08d4c8098b233184b838c20c825a40cb1
VGanpathiRaja <102245434+VGanpathiRaja@users.noreply.github.com>
2024-04-17 11:58:18 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
+import { Routes,Route } from "react-router-dom";
 import Login from "./Components/Login";
 import Signup from "./Components/Signup";
 import Profile from "./Components/Profile";
@@ -7,13 +8,13 @@ import Profile from "./Components/Profile";
 function App(){
   return(
     <div>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path="/" element={< Login/>}/>
               <Route path="/Signup" element={<Signup />}/>
               <Route path="/Profile" element={<Profile />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
     </div>
   )
 }
